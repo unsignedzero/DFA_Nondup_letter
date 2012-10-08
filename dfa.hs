a4bdeb53f16dc10fe26f9179db50d9257e108365
David Tran <unsignedzero@gmail.com>
2012-10-08 13:42:04 -0700

    DFA in interactive now

--- dfa.hs
+++ dfa.hs
@@ -4,14 +4,13 @@
 -- -- have different starting and ending characters
 -- -- any two neighboring characters are not identical
 
--- Check elem inputs
-
 h2 :: [Char] -> Bool
 h2 [] = True
 h2 (x:xs)
   | x == 'a' = h2aa xs
   | x == 'b' = h2bb xs
   | x == 'c' = h2cc xs
+  | otherwise = False
 
 h2aa :: [Char] -> Bool
 h2aa [] = False
@@ -19,6 +18,7 @@ h2aa (x:xs)
   | x == 'a' = False
   | x == 'b' = h2ab xs
   | x == 'c' = h2ac xs
+  | otherwise = False
 
 h2ab :: [Char] -> Bool
 h2ab [] = True
@@ -26,6 +26,7 @@ h2ab (x:xs)
   | x == 'a' = h2aa xs
   | x == 'b' = False
   | x == 'c' = h2ac xs
+  | otherwise = False
 
 h2ac :: [Char] -> Bool
 h2ac [] = True
@@ -33,6 +34,7 @@ h2ac (x:xs)
   | x == 'a' = h2aa xs
   | x == 'b' = h2ab xs
   | x == 'c' = False
+  | otherwise = False
 
 h2ba :: [Char] -> Bool
 h2ba [] = True
@@ -40,6 +42,7 @@ h2ba (x:xs)
   | x == 'a' = False
   | x == 'b' = h2bb xs
   | x == 'c' = h2bc xs
+  | otherwise = False
 
 h2bb :: [Char] -> Bool
 h2bb [] = False
@@ -47,6 +50,7 @@ h2bb (x:xs)
   | x == 'a' = h2ba xs
   | x == 'b' = False
   | x == 'c' = h2bc xs
+  | otherwise = False
 
 h2bc :: [Char] -> Bool
 h2bc [] = True
@@ -54,6 +58,7 @@ h2bc (x:xs)
   | x == 'a' = h2ba xs
   | x == 'b' = h2bb xs
   | x == 'c' = False
+  | otherwise = False
 
 h2ca :: [Char] -> Bool
 h2ca [] = True
@@ -61,6 +66,7 @@ h2ca (x:xs)
   | x == 'a' = False
   | x == 'b' = h2cb xs
   | x == 'c' = h2cc xs
+  | otherwise = False
 
 h2cb :: [Char] -> Bool
 h2cb [] = True
@@ -68,6 +74,7 @@ h2cb (x:xs)
   | x == 'a' = h2ca xs
   | x == 'b' = False
   | x == 'c' = h2cc xs
+  | otherwise = False
 
 h2cc :: [Char] -> Bool
 h2cc [] = False
@@ -75,3 +82,18 @@ h2cc (x:xs)
   | x == 'a' = h2ca xs
   | x == 'b' = h2cb xs
   | x == 'c' = False
+  | otherwise = False
+
+--Support Function
+print_bool :: Bool -> [Char]                                                              
+print_bool True = "True"
+print_bool False = "False"  
+
+-- Main Function
+main :: IO()
+main = do
+  putStrLn "Starting DFA_NonDupLetter Program"
+  putStrLn "Enter a string consisting of the letters a,b and c"
+  input_line <- getLine
+  putStrLn $ print_bool $ h2 input_line
+
